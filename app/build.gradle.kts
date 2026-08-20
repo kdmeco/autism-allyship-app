d96b4e13cdd84a5877f7fc325d9a66b613ae2db3
Megan Johnston <meganjohnston100@gmail.com>
2026-08-20 21:03:39 +0200

    add the google code scanner dependency for the ticket scanner

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.glide)
     implementation(libs.material)
     implementation(libs.zxing.core)
+    implementation(libs.play.services.code.scanner)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
